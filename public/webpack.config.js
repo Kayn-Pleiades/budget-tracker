385499d13bdbb19bd87e1ccff1fdacf98efe6e84
unknown <kayn.pleiades@gmail.com>
2021-05-21 16:09:21 -0400

    starting to flesh out webpack outline

--- public/webpack.config.js
+++ public/webpack.config.js
@@ -1,5 +1,15 @@
 // Needed Consts
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
 
 // Set up config
+const config = {
+    // Entry
+    // Output
+    // Mode
+    // Module
+    // Plugins
+}
 
 // Export
+module.exports = config;
